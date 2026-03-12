6c985ab01a7bf93fd1fdcd0639f8f32289d547c9
LAASYA REDDY N <laasyanagatham16@gmail.com>
2026-03-12 11:02:12 +0530

    Initial commit BookMyStayApp

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,21 @@
+
+/**
+ * BookMyStayApp
+ * Hotel Booking Management System
+ *
+ * @author Laasya
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Book My Stay App ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+
+    }
+
+}
